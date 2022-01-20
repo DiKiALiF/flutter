95d0f5141f82cbc42389ba4a95e367438606e542
DiKiALiF <diki_171@smkassalaambandung.sch.id>
2022-01-20 07:02:51 +0700

    hari ke 3

--- hari_ke-3/latihan/coba.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main() {
-  String? s = stdin.readLineSync();
-  if (s != null){
-    int n = int.parse(s);
-    print(n); // Or do whatever you want with your n value
-  }
-}
